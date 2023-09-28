39aa006c27afb68eddbabc8027782e0573d541a8
CARLOSbesserra <carloshcarvalho200999@gmail.com>
2023-09-28 11:30:37 -0300

    FelizNatal

--- app/src/main/java/com/example/feliznatal2023/MainActivity.kt
+++ app/src/main/java/com/example/feliznatal2023/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.feliznatal2023
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.feliznatal2023.ui.theme.FelizNatal2023Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,32 +24,32 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FelizNatal2023Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
     }
 }
 
+@Preview(showSystemUi = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun TextoCartão(){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
 
-@Preview(showBackground = true,
-    showSystemUi= true,
-    name = "My Priview ")
-@Composable
-fun GreetingPreview() {
-    FelizNatal2023Theme {
-        Greeting("Android")
+        Text(
+            text = "Feliz Natal 2023 !!!",
+            fontSize = 80.sp,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = "Para: Anne",
+            fontSize = 40.sp,
+
+        )
     }
+
 }
