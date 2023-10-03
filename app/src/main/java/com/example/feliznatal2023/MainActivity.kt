ce6f8bfab3b8e0b03fd29d1f5db2dfabb034e8ae
CARLOSbesserra <carloshcarvalho200999@gmail.com>
2023-10-03 10:30:51 -0300

    FelizNatal

--- app/src/main/java/com/example/feliznatal2023/MainActivity.kt
+++ app/src/main/java/com/example/feliznatal2023/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -21,6 +23,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.feliznatal2023.ui.theme.FelizNatal2023Theme
 
@@ -57,15 +60,20 @@ fun TextoCartão(){
             textAlign = TextAlign.Center,
             fontWeight = FontWeight.Black,
             fontFamily = FontFamily.Cursive,
+            lineHeight = 110.sp,
             color = Color.DarkGray
 
         )
         Text(
             text = "Para: Anne",
             fontSize = 40.sp,
+            textAlign = TextAlign.End,
             fontWeight = FontWeight.Bold,
             fontFamily = FontFamily.Cursive,
             color = Color.Red,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(end = 30.dp)
         )
     }
 
